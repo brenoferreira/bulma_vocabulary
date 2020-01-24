abea81b9ae6ab08279fef5cf0e55aa7b371bfec0
Breno Ferreira <bferreira@windowslive.com>
2020-01-24 17:26:10 -0300

    button stories

--- src/stories/buttons.stories.js
+++ src/stories/buttons.stories.js
@@ -1,3 +1,13 @@
 export default { title: 'Buttons' };
 
-export const Button = () => '<button class="button">Button</button>';
+export const Button = () => '<button class="button is-primary">Button</button>';
+
+export const ButtonInfo = () => '<button class="button is-info">Button</button>';
+
+export const ButtonDonate = () => '<button class="button donate">Button</button>';
+
+export const ButtonSuccess = () => '<button class="button is-success">Button</button>';
+
+export const ButtonWarning = () => '<button class="button is-warning">Button</button>';
+
+export const ButtonError = () => '<button class="button is-danger">Button</button>';
