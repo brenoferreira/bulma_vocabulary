44a3588782cce1077f252fec83bc4d51cd588d68
Breno Ferreira <bferreira@windowslive.com>
2020-01-27 11:24:35 -0300

    background colors

--- src/stories/colors.stories.js
+++ src/stories/colors.stories.js
@@ -1,10 +1,39 @@
-export default { title: 'Colors' };
+import { withKnobs, select } from '@storybook/addon-knobs';
 
-export const Primary = () => '<div class="box has-background-primary" style="width: 300px; height: 300px;"></div>';
-export const Info = () => '<div class="box has-background-info" style="width: 300px; height: 300px;"></div>';
-export const Link = () => '<div class="box has-background-link" style="width: 300px; height: 300px;"></div>';
-export const Success = () => '<div class="box has-background-success" style="width: 300px; height: 300px;"></div>';
-export const Warning = () => '<div class="box has-background-warning" style="width: 300px; height: 300px;"></div>';
-export const Error = () => '<div class="box has-background-danger" style="width: 300px; height: 300px;"></div>';
+export default { title: 'Colors', decorators: [withKnobs] };
 
+export const Context = () => {
+    const Context = {
+        Primary: 'primary',
+        Info: 'info',
+        Link: 'link',
+        Success: 'success',
+        Warning: 'warning',
+        Error: 'danger',
+    };
+      
+    const context = select('Context', Context, Context.Primary);
 
+    return `<div class="box has-background-${context}" style="width: 300px; height: 300px;"></div>`;
+};
+Context.story = { name: 'Context' };
+
+export const Brands = () => {
+    const Brands = {
+        Blue: 'blue',
+        Forest: 'forest',
+        Gold: 'gold',
+        Orange: 'orange',
+        Tomato: 'tomato',
+        Turquoise: 'turquoise',
+        DarkSlateGray: 'dark-slate-gray',
+        TomatoBrighter: 'tomato-brighter',
+        ForestBrighter: 'forest-brighter',
+        GoldBrighter: 'gold-brighter',
+    };
+    
+    const brands = select('Brand', Brands, Brands.Tomato);
+
+    return `<div class="box has-background-${brands}" style="width: 300px; height: 300px;"></div>`;
+};
+Brands.story = { name: 'Brands' };
