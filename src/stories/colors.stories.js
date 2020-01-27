faf42633cd5ed68b0211441dc898b18091576dce
Breno Ferreira <bferreira@windowslive.com>
2020-01-27 10:15:49 -0300

    more color stories

--- src/stories/colors.stories.js
+++ src/stories/colors.stories.js
@@ -1,5 +1,10 @@
 export default { title: 'Colors' };
 
 export const Primary = () => '<div class="box has-background-primary" style="width: 300px; height: 300px;"></div>';
+export const Info = () => '<div class="box has-background-info" style="width: 300px; height: 300px;"></div>';
+export const Link = () => '<div class="box has-background-link" style="width: 300px; height: 300px;"></div>';
+export const Success = () => '<div class="box has-background-success" style="width: 300px; height: 300px;"></div>';
+export const Warning = () => '<div class="box has-background-warning" style="width: 300px; height: 300px;"></div>';
+export const Error = () => '<div class="box has-background-danger" style="width: 300px; height: 300px;"></div>';
 
 
